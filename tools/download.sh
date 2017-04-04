bc35a79e6b020dbfa2375a7e68bda904888e3cee
Petr <petamaj@users.noreply.github.com>
2017-04-04 18:04:24 +0200

    Download.sh populated.

---
+++ tools/download.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/Mondego/SourcererCC.git
+git clone https://github.com/reactorlabs/js-tokenizer.git
+git clone https://github.com/reactorlabs/ght-pipeline.git
+git clone https://github.com/reactorlabs/sccpreprocessor.git
+git clone https://github.com/reactorlabs/js-reports.git
+git clone https://github.com/reactorlabs/clone_finder.git
